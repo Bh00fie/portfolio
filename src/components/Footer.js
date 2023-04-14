e9c4e8fb6f218d0072b51efff4c5bf7b7eeba12b
Abhi <Bh00fie@gmail.com>
2023-04-14 19:12:40 +0100

    updated Footer and created style.css for general styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './style.css'
 
 function Footer() {
   return (
-    <div>
+    <div className="footer">
       <p>Designed by Abhi - All right reserved 2023</p>
     </div>
   );
