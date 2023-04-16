a2846b3fc0202521a7a284672d51ae39427bcb97
Abhi <Bh00fie@gmail.com>
2023-04-16 22:02:50 +0100

    fixed footer to the bottom of the webpage using flex

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ import Project from './components/pages/Project';
 import CV from './components/pages/CV';
 import Contact from './components/pages/Contact';
 import About from './components/pages/About';
+import '../src/components/style.css'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavTabs />
-        {/* Wrap Route elements in a Routes component */}
-        <Routes>
-          {/* Define routes using the Route component to render different page components at different paths */}
-          {/* Define a default route that will render the Home component */}
-          <Route path="/" element={<Home />} />
-          <Route path="project" element={<Project />} />
-          <Route path="CV" element={<CV />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
+      <Router>
+        <div className='App'>
+          <NavTabs />
+          <Routes>
+              {/* Define routes using the Route component to render different page components at different paths */}
+              {/* Define a default route that will render the Home component */}
+              <Route path="/" element={<Home />} />
+              <Route path="project" element={<Project />} />
+              <Route path="CV" element={<CV />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="portfolioHome">
-      <img className="photoHome" src="https://placehold.co/400" alt="photoprofile" />
-      <div className="descriptionHome  shadow p-3 bg-white rounded">
-        <h3>Abhinandan Thour</h3>
-        <p> Front End Software Developer</p>
-        <div className="homeLinks">
-          <a href="https://github.com/Bh00fie" target='_blank' rel="noreferrer">Github</a>
-          <a href="https://github.com/Bh00fie" target='_blank' rel="noreferrer">LinkedIn</a>
+      <div className="portfolioHome">
+        <img className="photoHome" src="https://placehold.co/400" alt="photoprofile" />
+        <div className="descriptionHome  shadow p-3 bg-white rounded">
+          <h3>Abhinandan Thour</h3>
+          <p> Front End Software Developer</p>
+          <div className="homeLinks">
+            <a href="https://github.com/Bh00fie" target='_blank' rel="noreferrer">Github</a>
+            <a href="https://github.com/Bh00fie" target='_blank' rel="noreferrer">LinkedIn</a>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
