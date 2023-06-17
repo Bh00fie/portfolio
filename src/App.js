e3ed7ae5ab355f47545c614ff98a4707bf92af5a
Abhi <Bh00fie@gmail.com>
2023-06-18 00:59:54 +0100

    changed idea and removed navbar, keeping everything in one page now

--- src/App.js
+++ src/App.js
@@ -13,16 +13,21 @@ function App() {
   return (
       <Router>
         <div className='App'>
-          <NavTabs />
-          <Routes>
+          <Home/>
+          {/* <NavTabs /> */}
+          {/* <Routes> */}
               {/* Define routes using the Route component to render different page components at different paths */}
               {/* Define a default route that will render the Home component */}
-              <Route path="/" element={<Home />} />
+              {/* <Route path="/" element={<Home />} />
               <Route path="project" element={<Project />} />
               <Route path="CV" element={<CV />} />
               <Route path="about" element={<About />} />
-              <Route path="contact" element={<Contact />} />
-          </Routes>
+              <Route path="contact" element={<Contact />} /> */}
+          {/* </Routes>  */}
+          <Project/>
+          <CV />
+          <CV />
+          <CV />
           <Footer />
         </div>
       </Router>
