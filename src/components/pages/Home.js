74c85453c5ef120a6bdfb58dd0b92481b6609b45
Abhi <Bh00fie@gmail.com>
2023-06-18 15:57:34 +0100

    created images folder and put the photo profile of the user, linkedin link update to user's linkedin

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,13 +4,13 @@ function Home() {
   return (
     <div className="home">
       <div className="portfolioHome">
-        <img className="photoHome" src="https://placehold.co/400" alt="photoprofile" />
-        <div className="descriptionHome  shadow p-3 bg-white rounded">
+        <img className="photoHome" src="../images/photoprofile.png" alt="photoprofile" />
+        <div className="descriptionHome shadow p-3 bg-white rounded">
           <h3>Abhinandan Thour</h3>
-          <p> Front End Software Developer</p>
+          <p>Front End Software Developer</p>
           <div className="homeLinks">
-            <a href="https://github.com/Bh00fie" target='_blank' rel="noreferrer">Github</a>
-            <a href="https://github.com/Bh00fie" target='_blank' rel="noreferrer">LinkedIn</a>
+            <a href="https://github.com/Bh00fie" target="_blank" rel="noreferrer">Github</a>
+            <a href="https://www.linkedin.com/in/abhinandan-thour-42359b193/" target="_blank" rel="noreferrer">LinkedIn</a>
           </div>
         </div>
       </div>
