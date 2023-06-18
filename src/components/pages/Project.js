0d1ada9ed005ebf6dce351cc567d9ca2b267ddd6
Abhi <Bh00fie@gmail.com>
2023-06-18 18:35:38 +0100

    create function in Projects that creates cards based on an input

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,19 +1,31 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
 
+function generateCards(num) {
+  const cards = [];
+  
+  for (let i = 1; i <= num; i++) {
+    cards.push(
+      <div key={i} id={`card${i}`}>
+        <Card>
+          <Card.Img variant="top" src="https://placehold.co/400x300" />
+          <Card.Body>
+            <Card.Title>Project {i}</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+  
+  return cards;
+}
+
 function Project() {
   return (
     <div>
       <h1 className="sectionTitle">Projects</h1>
       <div id="projectsCard">
-        <div id="cardOne">
-          <Card>
-            <Card.Img variant="top" src="https://placehold.co/400x300" />
-            <Card.Body>
-              <Card.Title> Project 1 </Card.Title>
-            </Card.Body>
-          </Card>
-        </div>
+        {generateCards(2)} {/* Change the argument to the desired number of cards */}
       </div>
     </div>
   );
