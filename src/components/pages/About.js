27a82fa58617a7b109cf098a436a84682f2347d9
Abhi <Bh00fie@gmail.com>
2023-06-18 16:38:23 +0100

    installed Inter font (the same used in figma)

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1 className="text-center">Who's Abhi?</h1>
+      <h1 id="personalTitle" className="text-center">Who's Abhi?</h1>
       <p id="personalDescription">I'm an Aerospace and Mechanical Engineering student at the University of Southampton. With a background in Italy, Ireland, and the UK, I bring a diverse perspective. My passion for technology and improving lives led me to pursue Mechanical Engineering, but I've realized the increasing influence of software and now seek to transition into this field. Explore my portfolio to learn more about my academic achievements and projects as I embark on this exciting journey into software.</p>
     </div>
   );
