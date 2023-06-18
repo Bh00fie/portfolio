5d5c1d10dd961c3b053253b2ab73fbcbbab747ba
Abhi <Bh00fie@gmail.com>
2023-06-18 20:53:40 +0100

    created styling for cards to be at the center of the page and be responsive

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import './Project.css'; // Import CSS file for styling
 
 function generateCards(num) {
   const cards = [];
   
   for (let i = 1; i <= num; i++) {
     cards.push(
-      <div key={i} id={`card${i}`}>
+      <div key={i} className={`card ${i % 2 === 0 ? 'left' : 'right'}`}>
         <Card>
           <Card.Img variant="top" src="https://placehold.co/400x300" />
           <Card.Body>
@@ -24,8 +25,8 @@ function Project() {
   return (
     <div>
       <h1 className="sectionTitle">Projects</h1>
-      <div id="projectsCard">
-        {generateCards(2)} {/* Change the argument to the desired number of cards */}
+      <div id="projectsCard" className="cardsContainer">
+        {generateCards(4)} {/* Change the argument to the desired number of cards */}
       </div>
     </div>
   );
